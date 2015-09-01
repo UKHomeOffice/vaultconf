3a6b2c0205f13bdd3b2f9427878f16555cb547fa
Tim Gent <tim.gent@gmail.com>
2015-09-01 10:40:48 +0100

    Cucumber tests now start and shutdown vault server automatically

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,9 +10,23 @@ Before do
   @puts = true
   @original_rubylib = ENV['RUBYLIB']
   ENV['RUBYLIB'] = LIB_DIR + File::PATH_SEPARATOR + ENV['RUBYLIB'].to_s
+  setup_vault_server
+
+end
+
+def setup_vault_server
+  @vault_server = fork do
+    exec 'vault server -dev'
+  end
+  sleep 2
+  `vault auth-enable -address=http://localhost:8200 userpass`
+  `vault write -address=http://localhost:8200 auth/userpass/users/user password=password policies=root`
 end
 
 After do
   ENV['RUBYLIB'] = @original_rubylib
   Vault.sys.policies.select{|policy| policy != 'root'}.each { |policy| Vault.sys.delete_policy(policy) }
+  puts @pid
+  Process.kill 'SIGTERM', @vault_server
 end
+
